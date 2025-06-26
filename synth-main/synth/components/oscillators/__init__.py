31fe957d00f92aba9733cdaa83524ebd5b99b141
Florencia Fontana Walser <72945552+florfontana02@users.noreply.github.com>
2025-06-26 10:57:13 -0300

    Update __init__.py with WavetableOscillator

--- synth-main/synth/components/oscillators/__init__.py
+++ synth-main/synth/components/oscillators/__init__.py
@@ -2,4 +2,5 @@ from .oscillators import SineOscillator
 from .oscillators import SquareOscillator
 from .oscillators import SawtoothOscillator
 from .oscillators import TriangleOscillator
+from .oscillators import WavetableOscillator
 from .modulated_oscillator import ModulatedOscillator
